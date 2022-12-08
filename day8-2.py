7e3784b693292903520c0f466bfdd27e3e1f5cf6
Danny Lin <danny@kdrag0n.dev>
2022-12-07 21:13:17 -0800

    day 8

--- day8-2.py
+++ day8-2.py
@@ -23,35 +23,39 @@ while True:
     break
 import copy
 g2 = copy.deepcopy(grid)
+scores = []
 for y, row in enumerate(grid):
     for x, cell in enumerate(row):
         cur_h = cell
         dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         vis = False
+        sc = 1
         for dx, dy in dirs:
+            i = 0
             x2, y2 = x, y
             print('  check dir', dx, dy)
-            if vis: break
             blocked = False
             while True:
                 x2 += dx
                 y2 += dy
                 if x2 < 0 or y2 < 0 or x2 >= len(row) or y2 >= len(grid):
                     break
+                i += 1
                 print('    check cell', x2, y2, grid[y2][x2])
                 if grid[y2][x2] >= cur_h:
                     print('      blocked')
                     blocked = True
                     break
-            if not blocked:
-                vis = True
-                break
+            #if not blocked:
+            sc *= i
+        scores += [sc]
         print(x, y, cell, vis)
         if vis:
             total += 1
             g2[y][x] = 'X'
 
 print('\n'.join([''.join([str(c) for c in r]) for r in g2]))
+print(max(scores))
 
 print(f'Total: {total}')
 print(f'Result: {result}')
